da5bb10628f1affa81f6b625fe5309d99180d49e
Megha kickdrum <megha.goel@kickdrumtech.com>
2021-08-10 16:33:19 +0530

    new changes

--- frontend/assignments/3/Assignment_3/client.js
+++ frontend/assignments/3/Assignment_3/client.js
@@ -1,40 +1,43 @@
-const socket = io('http://localhost:8000');
+const socket = io('http://localhost:8000');     // it is working on port:8000
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
-const messageContainer = document.querySelector(".container");
+const messageContainer = document.querySelector(".container");   
 
 var audio = new Audio('trinsound.mp3');
 
-const append = (message, position)=>{
+//function to append messages to the message container
+const append = (message, position)=>{    //position is the left or right position of the message in the container 
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
-    messageContainer.append(messageElement);
-    if(position == 'left')
+    messageContainer.append(messageElement);  //we add the new messages to the message container
+    if(position == 'left')   // so that we listen to the notification sound only when we receive a message and not when we send a message
     {
-        audio.play();
+        audio.play();  // play the notification sound   
+        messageElement.style.backgroundColor = "rgb(132, 68, 161)";
+        
     }
 
 }
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e)=>{           // if the form is submitted a function runs that emits the send event
     e.preventDefault();
     const message = messageInput.value ;
     append(`You: ${message}`, 'right');
-    socket.emit('send', message);
-    messageInput.value = ''
+    socket.emit('send', message);  // tells the node server that we are sending a message
+    messageInput.value = ''   // to make the input area blank after sending the message
 })
 
 const name  = prompt("Enter your name to join");
-socket.emit('new-user-joined');
+socket.emit('new-user-joined', name); // here we emit an event named new-user-joined
 
-socket.on('user-joined', name =>{
-    append(`${name} joined the chat`, 'right')
+socket.on('user-joined', name =>{       // here we listen to the event user-joined which was emitted and
+    append(`${name} joined the chat`, 'right')  // run a function which appends in the message container the name of user who joined the chat
 })
 
-socket.on('receive', data =>{
+socket.on('receive', data =>{         // whenever we listen to a receive event , we append that message in 
     append(`${data.name}: ${data.message}`, 'left')
 })
 
--- frontend/assignments/3/Assignment_3/nodeServer/index.js
+++ frontend/assignments/3/Assignment_3/nodeServer/index.js
@@ -1,22 +1,27 @@
 // Node server which will handle socket io connections
 
-const io = require('socket.io')(8000);
+// const io = require('socket.io')(8000);
+const io = require('socket.io')(8000, {
+    cors: {
+      origin: '*',
+    }
+  });
 
 const users = {};
 
 io.on('connection', socket =>{
-    socket.on('new-user-joined', name =>{
-        console.log("New user", name);
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
+    socket.on('new-user-joined', name =>{      // here the node Server accepts the event named new-user-joined
+        console.log("New user", name);        // if new-user-joined event is found, it will add the user name to the users list
+        users[socket.id] = name;                // provides unique id to the socket (the user)
+        socket.broadcast.emit('user-joined', name);  //to emit event (that a new user has joined)to all other users except the new user who joined
     });
 
-    socket.on('send', message =>{
+    socket.on('send', message =>{       //if event is - send, others will receive the message
         socket.broadcast.emit('receive', {message: message,name: users[socket.id]})
     });
     
     socket.on('disconnect', message =>{
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id];
+        socket.broadcast.emit('left', users[socket.id]);   // to inform other users if a user leaves the chat and tell his name
+        delete users[socket.id]; // to delete the user who left ,from our users list
     });
 })
